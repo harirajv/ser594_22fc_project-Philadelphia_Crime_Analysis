c9bfa5c830a93017b75cb92be1ac179c72a0bd1d
harirajv <harirajv@gmail.com>
2022-12-04 20:38:20 -0700

    fixed train test split

--- wf_ml_evaluation.py
+++ wf_ml_evaluation.py
@@ -8,30 +8,50 @@ from wf_ml_training import create_models
 from wf_ml_prediction import run_predictions, perform_prediction
 
 data = pd.read_csv("data_processed/processed_data.csv")
-removable_features = ['Unnamed: 0', 'the_geom', 'cartodb_id', 'the_geom_webmercator', 'objectid', 'dispatch_date_time',
-                      'dispatch_date', 'dispatch_time', 'hour_', 'dc_key', 'location_block',
-                      'text_general_code', 'point_x', 'point_y']
-features = list(data.columns)
-features = [ft for ft in features if ft not in removable_features]
 
-y = data["ucr_general"]
+features = ['dc_dist_1', 'dc_dist_2', 'dc_dist_3', 'dc_dist_5',
+       'dc_dist_6', 'dc_dist_7', 'dc_dist_8', 'dc_dist_9', 'dc_dist_12',
+       'dc_dist_14', 'dc_dist_15', 'dc_dist_16', 'dc_dist_17', 'dc_dist_18',
+       'dc_dist_19', 'dc_dist_22', 'dc_dist_24', 'dc_dist_25', 'dc_dist_26',
+       'dc_dist_35', 'dc_dist_39', 'dc_dist_77', 'psa_1', 'psa_2', 'psa_3',
+       'psa_4', 'psa_A', 'psa_Unknown', 'dispatch_day_name_Friday',
+       'dispatch_day_name_Monday', 'dispatch_day_name_Saturday',
+       'dispatch_day_name_Sunday', 'dispatch_day_name_Thursday',
+       'dispatch_day_name_Tuesday', 'dispatch_day_name_Wednesday', 'lat', 'lng',
+       'dispatch_day', 'dispatch_month', 'dispatch_year', 'dispatch_time_seconds',
+       'dispatch_hour', 'dispatch_minute', 'dispatch_second']
+target = 'ucr_general'
 
-X = data[features].drop(["ucr_general"], axis=1)
+tscv = TimeSeriesSplit()
+train_splits = []
+test_splits = []
 
-# 80% training data, 20% test data
-train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=0, test_size=0.2, shuffle=False)
+for train_index, test_index in tscv.split(data):
+    X_train, X_test = data.iloc[train_index,:], data.iloc[test_index,:]
+    train_splits.append(X_train)
+    test_splits.append(X_test)
+
+train_data = pd.concat(train_splits)
+test_data = pd.concat(test_splits)
+
+print("Train data shape: ", train_data.shape)
+print("Test data shape: ", test_data.shape)
+
+train_X = train_data.drop(['ucr_general'], axis = 1)[features]
+train_y = train_data['ucr_general']
+
 training_data = pd.concat([train_X, train_y], axis=1)
 
 training_data.to_csv("data_processed/training_data.csv")
+
+test_X = test_data.drop(['ucr_general'], axis = 1)[features]
+test_y = test_data['ucr_general']
 test_X.to_csv("data_processed/testing_data.csv")
 
 create_models(train_X, train_y)
 
 performance = [["Model", "Accuracy", "Precision"]]
 
-# pred = perform_prediction(test_X, "./models/knn_model_8.pkl")
-# print(accuracy_score(test_y, pred, normalize=True))
-# print(precision_score(test_y, pred, average='macro'))
 predictions = run_predictions(test_X)
 for model, pred in predictions.items():
     performance.append([
--- wf_ml_training.py
+++ wf_ml_training.py
@@ -1,8 +1,25 @@
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV, TimeSeriesSplit
+from sklearn.neighbors import KNeighborsClassifier
 import os
 import pickle
 
+
+def create_best_model(X, y):
+
+    model = RandomForestClassifier()
+    param_search = {
+        'n_estimators': [50, 100, 200, 400],
+        'max_features': ['sqrt', 'log2'],
+        'max_depth': [4, 8, 16, 32, 64]
+    }
+    tscv = TimeSeriesSplit(n_splits=4)
+    gsearch = GridSearchCV(estimator=model, cv=tscv, param_grid=param_search, scoring='accuracy')
+    gsearch.fit(X, y)
+    best_model = gsearch.best_estimator_
+    best_parameters = gsearch.best_params_
+    print(f"Best parameters for {model}: ", best_parameters)
+    return best_model
 
 def create_models(X, y):
     print("Creating model with KNeighborsClassifier with 8 neighbors")
@@ -17,16 +34,18 @@ def create_models(X, y):
     knn_model_14 = KNeighborsClassifier(n_neighbors=14)
     knn_model_14.fit(X, y)
 
-    print("Creating model with RandomForestClassifier")
-    rf_model = RandomForestClassifier(n_estimators=200, max_depth=32, random_state=0)
-    rf_model.fit(X, y)
+    print("Grid search for best parameters of RandomForestClassifier")
+    rf_best_model = create_best_model(X, y)
 
     outdir = './models'
     if not os.path.exists(outdir):
         os.mkdir(outdir)
 
-    with open("models/knn_model_8.pkl", "wb") as knn_file_8,open("models/knn_model_10.pkl", "wb") as knn_file_10,open("models/knn_model_14.pkl", "wb") as knn_file_14, open("models/rf_model.pkl", "wb") as rf_file:
+    with open("models/knn_model_8.pkl", "wb") as knn_file_8,\
+            open("models/knn_model_10.pkl", "wb") as knn_file_10,\
+            open("models/knn_model_14.pkl", "wb") as knn_file_14,\
+            open("models/rf_best_model.pkl", "wb") as rf_best_file:
         pickle.dump(knn_model_8, knn_file_8)
         pickle.dump(knn_model_10, knn_file_10)
         pickle.dump(knn_model_14, knn_file_14)
-        pickle.dump(rf_model, rf_file)
+        pickle.dump(rf_best_model, rf_best_file)
